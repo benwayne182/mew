2a97089dc2b7600cdb3b3fa45e4592a714b8be7a
Benoît Sinivassin <benoit.sinivassin@laas.fr>
2015-04-17 19:23:32 +0200

    2nd commit CMakeLists.txt customizer.cpp and plugin added

---
+++ customizer.cpp
@@ -0,0 +1,109 @@
+#include <hrpModel/BodyCustomizerInterface.h>
+#include <fstream>
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+#define DLL_EXPORT __declspec(dllexport)
+#else 
+#define DLL_EXPORT 
+#endif /* Windows */
+
+#if defined(HRPMODEL_VERSION_MAJOR) && defined(HRPMODEL_VERSION_MINOR)
+#if HRPMODEL_VERSION_MAJOR >= 3 && HRPMODEL_VERSION_MINOR >= 1
+#define NS_HRPMODEL hrp
+#endif
+#endif
+
+#ifndef NS_HRPMODEL
+#define NS_HRPMODEL OpenHRP
+#endif
+
+using namespace std;
+using namespace NS_HRPMODEL;
+
+static BodyInterface* bodyInterface = 0;
+
+static BodyCustomizerInterface bodyCustomizerInterface;
+
+struct JointValSet
+{
+    double* valuePtr;
+    double* velocityPtr;
+    double* torqueForcePtr;
+};
+
+struct Customizer
+{
+    BodyHandle bodyHandle;
+    JointValSet jointValSet;
+
+    double springT;
+    double dampingT;
+};
+
+static const char** getTargetModelNames()
+{
+    static const char* names[] = { 
+        "crankGear",
+        0 };
+	
+    return names;
+}
+
+static BodyCustomizerHandle create(BodyHandle bodyHandle, const char* modelName)
+{
+    Customizer* customizer = 0;
+	
+    string name(modelName);
+    if(name == "crankGear"){
+        customizer = new Customizer;
+        customizer->bodyHandle = bodyHandle;
+        customizer->springT = 0.0;    
+        customizer->dampingT = 1.0e1;
+        int jointIndex = bodyInterface->getLinkIndexFromName(bodyHandle, "CRANK_GEAR");
+        if(jointIndex >=0 ){
+            JointValSet& jointValSet = customizer->jointValSet;
+            jointValSet.valuePtr = bodyInterface->getJointValuePtr(bodyHandle, jointIndex);
+            jointValSet.velocityPtr = bodyInterface->getJointVelocityPtr(bodyHandle, jointIndex);
+            jointValSet.torqueForcePtr = bodyInterface->getJointForcePtr(bodyHandle, jointIndex);
+        }
+    }
+
+    return static_cast<BodyCustomizerHandle>(customizer);
+}
+
+
+static void destroy(BodyCustomizerHandle customizerHandle)
+{
+    Customizer* customizer = static_cast<Customizer*>(customizerHandle);
+    if(customizer){
+        delete customizer;
+    }
+}
+
+static void setVirtualJointForces(BodyCustomizerHandle customizerHandle)
+{	
+    ofstream file("test.txt", ofstream::out | ofstream::app);
+    Customizer* customizer = static_cast<Customizer*>(customizerHandle);
+    JointValSet& trans = customizer->jointValSet;
+    *(trans.torqueForcePtr) = - customizer->springT * (*trans.valuePtr) - customizer->dampingT * (*trans.velocityPtr);
+    if (file.is_open()){
+    	file << *(trans.torqueForcePtr);
+	file.close();
+    }
+}
+
+
+extern "C" DLL_EXPORT
+NS_HRPMODEL::BodyCustomizerInterface* getHrpBodyCustomizerInterface(NS_HRPMODEL::BodyInterface* bodyInterface_)
+{
+    bodyInterface = bodyInterface_;
+
+    bodyCustomizerInterface.version = NS_HRPMODEL::BODY_CUSTOMIZER_INTERFACE_VERSION;
+    bodyCustomizerInterface.getTargetModelNames = getTargetModelNames;
+    bodyCustomizerInterface.create = create;
+    bodyCustomizerInterface.destroy = destroy;
+    bodyCustomizerInterface.initializeAnalyticIk = 0;
+    bodyCustomizerInterface.calcAnalyticIk = 0;
+    bodyCustomizerInterface.setVirtualJointForces = setVirtualJointForces;
+
+    return &bodyCustomizerInterface;
+}
